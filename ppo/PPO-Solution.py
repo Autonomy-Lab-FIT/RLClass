0f3910364378e05803776b516a4eb5112a133650
gnehma2020 <gnehma2020@my.fit.edu>
2026-04-21 17:22:32 -0400

    added PPO solution to repo

--- ppo/PPO-Solution.py
+++ ppo/PPO-Solution.py
@@ -11,10 +11,13 @@ import torch.nn.functional as F
 from torch.distributions import Normal
 import matplotlib.pyplot as plt
 
+# from collections import namedtuple
+# import random
+
 from utils import str2bool, plot_training_score, Action_adapter, Reward_adapter, evaluate_policy
 
 class Critic(nn.Module):
-	def __init__(self, state_dim, net_width):
+	def __init__(self, state_dim,net_width):
 		super(Critic, self).__init__()
 		
         # neural network architecture for the critic
@@ -37,7 +40,7 @@ class Critic(nn.Module):
 
     # forward pass through the critic network
 	def forward(self, state):
-		v = self.critic(state) 
+		v = self.critic(state)
 		return v
 
 # actor with gaussian distribution (continuous action space)
@@ -53,17 +56,15 @@ class Actor(nn.Module):
 			nn.Linear(net_width, action_dim)
         )
 
-		self.logstd = nn.Parameter(torch.zeros(action_dim))
-		
+		self.logstd = nn.Parameter(torch.zeros(action_dim))  # learnable log σ
 
 	def forward(self, state):
-		mu = self.mu_head(state)
-		sigma = torch.exp(self.logstd)
-	
-		return mu, sigma
+		mu = self.mu_head(state)         # unbounded
+		sigma = torch.exp(self.logstd)   # stable, constant per action dim
+		return mu,sigma
 
 	def get_dist(self, state):
-		mu, sigma = self.forward(state)
+		mu,sigma = self.forward(state)
 		dist = Normal(mu,sigma)
 		return dist
 
@@ -75,17 +76,16 @@ class PPO_agent(object):
 	def __init__(self, **kwargs):
 		# Init hyperparameters for PPO agent, just like "self.gamma = opt.gamma, self.lambd = opt.lambd, ..."
 		self.__dict__.update(kwargs)
-		
-		''' Build Actor '''
+
+        # Build Actor
 		self.actor = Actor(self.state_dim, self.action_dim, self.net_width).to(self.dvc)
 		self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=self.a_lr)
-		
-		'''Build Critic'''
+
+		# Build Critic
 		self.critic = Critic(self.state_dim, self.net_width).to(self.dvc)
 		self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=self.c_lr)
 
-		
-		'''Build Replay Buffer'''
+		# Build Replay buffer
 		self.s_buffer = np.zeros((self.T_horizon, self.state_dim),dtype=np.float32)
 		self.a_buffer = np.zeros((self.T_horizon, self.action_dim),dtype=np.float32)
 		self.r_buffer = np.zeros((self.T_horizon, 1),dtype=np.float32)
@@ -93,23 +93,20 @@ class PPO_agent(object):
 		self.logprob_a_buffer = np.zeros((self.T_horizon, self.action_dim),dtype=np.float32)
 		self.done_buffer = np.zeros((self.T_horizon, 1),dtype=np.bool_)
 		self.dw_buffer = np.zeros((self.T_horizon, 1),dtype=np.bool_)
-		
 
-	def select_action(self, _ , deterministic):
+	def select_action(self, state, deterministic):
 		with torch.no_grad():
 			state = torch.FloatTensor(state.reshape(1, -1)).to(self.dvc)
 			if deterministic:
 				# only used when evaluate the policy. Making the performance more stable
 				a = self.actor.deterministic_act(state)
-			
-				return a.cpu().numpy()[0], None
+				return a.cpu().numpy()[0], None  # action is in shape (adim, 0)
 			else:
 				# only used when interact with the env
 				dist = self.actor.get_dist(state)
 				a = dist.sample()
-				log_prob_a = dist.log_prob(a).cpu().numpy().flatten()
-				
-				return a.cpu().numpy()[0], log_prob_a 
+				logprob_a = dist.log_prob(a).cpu().numpy().flatten()
+				return a.cpu().numpy()[0], logprob_a # both are in shape (adim, 0)
 
 
 	def train(self):
@@ -126,32 +123,33 @@ class PPO_agent(object):
 
 		''' Use TD+GAE+LongTrajectory to compute Advantage and TD target'''
 		with torch.no_grad():
-			'''calculate values at current and next timestep'''
 			vs = self.critic(s)
 			vs_ = self.critic(s_next)
 
 			'''dw for TD_target and Adv'''
-			deltas = r + self.gamma * vs_ * (~dw) - vs
+			deltas = r + self.gamma * vs_ * (~dw) - vs # for the states that are not done
 			deltas = deltas.cpu().flatten().numpy()
-
 			adv = [0]
+
 			'''done for GAE'''
 			# calculate GAE backwards - with [::-1] indexing
 			for dlt, mask in zip(deltas[::-1], done.cpu().flatten().numpy()[::-1]):
-				advantage = dlt + self.gamma * self.lambd * (~mask) * adv[-1]
+				advantage = dlt + self.gamma * self.lambd * adv[-1] * (~mask)
 				adv.append(advantage)
-			adv.reverse()
-			adv = copy.deepcopy(adv[0:-1])
+			adv.reverse() # reverse to original order
+			adv = copy.deepcopy(adv[0:-1]) # remove extra zero
 			adv = torch.tensor(adv).unsqueeze(1).float().to(self.dvc)
-
 			td_target = adv + vs
 			adv = (adv - adv.mean()) / ((adv.std()+1e-4))  #sometimes helps
+
 
 		"""Slice long trajectopy into short trajectory and perform mini-batch PPO update"""
 		a_optim_iter_num = int(math.ceil(s.shape[0] / self.a_optim_batch_size))
 		c_optim_iter_num = int(math.ceil(s.shape[0] / self.c_optim_batch_size))
 		for i in range(self.K_epochs):
 			
+			# batch = self.buffer.sample(self.a_optim_batch_size)
+
 			#Shuffle the trajectory, Good for training
 			perm = np.arange(s.shape[0])
 			np.random.shuffle(perm)
@@ -162,50 +160,37 @@ class PPO_agent(object):
 			'''update the actor'''
 			# loop through all the batches of data
 			for i in range(a_optim_iter_num):
-				'''get the batch data'''
+				# get the specific batches
 				index = slice(i * self.a_optim_batch_size, min((i + 1) * self.a_optim_batch_size, s.shape[0]))
 				s_batch = s[index]
 				a_batch = a[index]
 				logprob_a_batch = logprob_a[index]
 				adv_batch = adv[index]
-
+				
 				distribution = self.actor.get_dist(s_batch)
 				dist_entropy = distribution.entropy().sum(1, keepdim=True)
-				
-				'''calculate log probablity ratio'''
-				logprob_a_new = distribution.log_prob(a_batch)
-				ratio = torch.exp(logprob_a_new.sum(1,keepdim=True) - logprob_a_batch.sum(1,keepdim=True))
+				logprob_a_now = distribution.log_prob(a_batch)
+				ratio = torch.exp(logprob_a_now.sum(1,keepdim=True) - logprob_a_batch.sum(1,keepdim=True))  # a/b == exp(log(a)-log(b))
 
-
-				'''calculate CLIP loss parts'''
 				surr1 = ratio * adv_batch
 				surr2 = torch.clamp(ratio, 1 - self.clip_rate, 1 + self.clip_rate) * adv_batch
 				a_loss = -torch.min(surr1, surr2) - self.entropy_coef * dist_entropy
-				a_loss = a_loss.mean()
 
-				'''backprop and optimise'''
 				self.actor_optimizer.zero_grad()
-				a_loss.backward()
+				a_loss.mean().backward()
 				torch.nn.utils.clip_grad_norm_(self.actor.parameters(), 40)
 				self.actor_optimizer.step()
 
 			'''update the critic'''
 			for i in range(c_optim_iter_num):
-				'''get the batch data'''
-				index = slice(i * self.a_optim_batch_size, min((i + 1) * self.a_optim_batch_size, s.shape[0]))
-				s_batch = s[index]
+				index = slice(i * self.c_optim_batch_size, min((i + 1) * self.c_optim_batch_size, s.shape[0]))
 				td_target_batch = td_target[index]
-				v = self.critic(s_batch)
-
-				'''calculate critic loss'''
-				c_loss = (v - td_target_batch).pow(2).mean()
-
-				'''apply L2 regularisation ONLY on weights'''
+				s_batch = s[index]
+				c_loss = (self.critic(s_batch) - td_target_batch).pow(2).mean()
 				for name,param in self.critic.named_parameters():
 					if 'weight' in name:
 						c_loss += param.pow(2).sum() * self.l2_reg
-						
-				'''backprop and optimise'''
+
 				self.critic_optimizer.zero_grad()
 				c_loss.backward()
 				self.critic_optimizer.step()
@@ -228,10 +213,11 @@ class PPO_agent(object):
 		self.critic.load_state_dict(torch.load("./model/{}_q_critic{}.pth".format(EnvName, timestep), map_location=self.dvc))
 
 
+
 '''Hyperparameter Setting'''
 parser = argparse.ArgumentParser()
 parser.add_argument('--dvc', type=str, default='cpu', help='running device: cuda or cpu')
-parser.add_argument('--EnvIdex', type=int, default=1, help='PV1, Lch_Cv2, Humanv4, HCv4, BWv3, BWHv3')
+parser.add_argument('--EnvIdex', type=int, default=0, help='PV1, Lch_Cv2, Humanv4, HCv4, BWv3, BWHv3')
 parser.add_argument('--write', type=str2bool, default=False, help='Use SummaryWriter to record the training')
 parser.add_argument('--render', type=str2bool, default=False, help='Render or Not')
 parser.add_argument('--Loadmodel', type=str2bool, default=False, help='Load pretrained model or Not')
@@ -240,7 +226,7 @@ parser.add_argument('--ModelIdex', type=int, default=100, help='which model to l
 parser.add_argument('--seed', type=int, default=0, help='random seed')
 parser.add_argument('--T_horizon', type=int, default=2048, help='lenth of long trajectory')
 parser.add_argument('--Distribution', type=str, default='Beta', help='Should be one of Beta ; GS_ms  ;  GS_m')
-parser.add_argument('--Max_train_steps', type=int, default=int(1e6), help='Max training steps')
+parser.add_argument('--Max_train_steps', type=int, default=int(2.5e6), help='Max training steps')
 parser.add_argument('--save_interval', type=int, default=int(5e5), help='Model saving interval, in steps.')
 parser.add_argument('--eval_interval', type=int, default=int(5e3), help='Model evaluating interval, in steps.')
 
@@ -260,6 +246,7 @@ opt = parser.parse_args()
 opt.dvc = torch.device(opt.dvc) # from str to torch.device
 print(opt)
 
+
 def plot_training_score(scores, steps, filename='training_score.png'):
     plt.figure(figsize=(10, 6))
     plt.plot(steps, scores, marker='o', linestyle='-', color='b')
@@ -271,20 +258,20 @@ def plot_training_score(scores, steps, filename='training_score.png'):
     plt.close()
 
 def main():
-    EnvName = ['r', 'LunarLanderContinuous-v2']
-    BrifEnvName = ['r', 'LLdV2']
-
-    '''Build Env'''
+    EnvName = ['Pendulum-v1','LunarLanderContinuous-v2','Humanoid-v4','HalfCheetah-v4','BipedalWalker-v3','BipedalWalkerHardcore-v3']
+    BrifEnvName = ['PV1', 'LLdV2', 'Humanv4', 'HCv4','BWv3', 'BWHv3']
+	
+    # Build Env
     env = gym.make(EnvName[opt.EnvIdex], render_mode = "human" if opt.render else None)
-	eval_env = gym.make(EnvName[opt.EnvIdex])
-	opt.state_dim = env.observation_space.shape[0]
-	opt.action_dim = env.action_space.shape[0]
-	opt.max_action = float(env.action_space.high[0])
+    eval_env = gym.make(EnvName[opt.EnvIdex])
+    opt.state_dim = env.observation_space.shape[0]
+    opt.action_dim = env.action_space.shape[0]
+    opt.max_action = float(env.action_space.high[0])
+    opt.max_steps = env._max_episode_steps
+    print('Env:',EnvName[opt.EnvIdex],'  state_dim:',opt.state_dim,'  action_dim:',opt.action_dim,
+          '  max_a:',opt.max_action,'  min_a:',env.action_space.low[0], 'max_steps', opt.max_steps)
 
-    # print('Env:',EnvName[opt.EnvIdex],'  state_dim:',opt.state_dim,'  action_dim:',opt.action_dim,
-        #   '  max_a:',opt.max_action,'  min_a:',env.action_space.low[0], 'max_steps', opt.max_steps)
-
-    '''Seed Everything'''
+    # Seed Everything
     env_seed = opt.seed
     torch.manual_seed(opt.seed)
     torch.cuda.manual_seed(opt.seed)
@@ -292,73 +279,71 @@ def main():
     torch.backends.cudnn.benchmark = False
     print("Random Seed: {}".format(opt.seed))
 
-    '''Use plotting to record training curves'''
+    # Use plotting to record training curves
     scores_list = []
     steps_list = []
     plot_filename = 'logs/training_score.png'
     os.makedirs(os.path.dirname(plot_filename), exist_ok=True)
     
 
-    '''Create PPO agent and directory to store'''
+    # Create PPO agent and directory to store
     if not os.path.exists('model'): os.mkdir('model')
-    agent = PPO_agent(**vars())
-    if opt.Loadmodel: 
-		agent.load(BrifEnvName[opt.EnvIdex], opt.ModelIdex)
-    '''Run Training loop or Eval'''
+    agent = PPO_agent(**vars(opt)) # transfer opt to dictionary, and use it to init PPO_agent
+    if opt.Loadmodel: agent.load(BrifEnvName[opt.EnvIdex], opt.ModelIdex)
+
     if opt.render:
         while True:
-			ep_r = evaluate_policy(env, agent, opt.max_action, 1)
-			print(f'Env:{EnvName[opt.EnvIdex]}, Episode Reward:{ep_r}')
+            ep_r = evaluate_policy(env, agent, opt.max_action, 1)
+            print(f'Env:{EnvName[opt.EnvIdex]}, Episode Reward:{ep_r}')
     else:
-        traj_length, total_steps = 0, 0
+        traj_lenth, total_steps = 0, 0
         while total_steps < opt.Max_train_steps:
-			'''Reset Env and initialise'''
-			s, info = env.reset(seed=env_seed)
-			env_seed += 1
-			done = False
-			'''Interact & train'''
-			while not done:
-				'''Interact with Env'''
-				a, logprob_a = agent.select_action(s, deterministic=False)
-				act = Action_adapter(a,opt.max_action) #[0,1] to [-max,max]
-				s_next, r, dw, tr, info = env.step(act) # dw: dead&win; tr: truncated
+            s, info = env.reset(seed=env_seed) # Do not use opt.seed directly, or it can overfit to opt.seed
+            env_seed += 1
+            done = False
+
+            '''Interact & trian'''
+            while not done:
+                '''Interact with Env'''
+                a, logprob_a = agent.select_action(s, deterministic=False) # use stochastic when training
+                act = Action_adapter(a,opt.max_action) #[0,1] to [-max,max]
+                s_next, r, dw, tr, info = env.step(act) # dw: dead&win; tr: truncated
                 r = Reward_adapter(r, opt.EnvIdex)
                 done = (dw or tr)
 
-				'''Store the current transition'''
-				agent.put_data(s, a, r, s_next, logprob_a, done, dw, idx = traj_length)
-				s = s_next
-
-				'''Fill initial entry of scores'''
-				if total_steps == 0:
+                '''Store the current transition'''
+                agent.put_data(s, a, r, s_next, logprob_a, done, dw, idx = traj_lenth)
+                s = s_next
+				
+                '''Fill initial entry of scores'''
+                if total_steps == 0:
                     steps_list.append(total_steps)
                     score = evaluate_policy(eval_env, agent, opt.max_action, turns=3)
                     scores_list.append(score)
-				
-				traj_length += 1
-				total_steps += 1
-		
-				'''Update if its time'''
-				if traj_length % opt.T_horizon == 0:
-					agent.train()
-					traj_length = 0
-				
-				'''Record & log'''
-				if total_steps % opt.eval_interval == 0:
+					
+
+                traj_lenth += 1
+                total_steps += 1
+
+                '''Update if its time'''
+                if traj_lenth % opt.T_horizon == 0:
+                    agent.train()
+                    traj_lenth = 0
+
+                '''Record & log'''
+                if total_steps % opt.eval_interval == 0:
                     score = evaluate_policy(eval_env, agent, opt.max_action, turns=3) # evaluate the policy for 3 times, and get averaged result
                     scores_list.append(score)
                     steps_list.append(total_steps)
                     plot_training_score(scores_list, steps_list, plot_filename)
                     print('EnvName:',EnvName[opt.EnvIdex],'seed:',opt.seed,'steps: {}k'.format(int(total_steps/1000)),'score:', score)
-				
-				
-				'''Save model'''
-				if total_steps % opt.save_interval == 0:
-					agent.save(BrifEnvName[opt.EnvIdex], int(total_steps/1000))
 
-		env.close()
-		eval_env.close()
+                '''Save model'''
+                if total_steps % opt.save_interval==0:
+                    agent.save(BrifEnvName[opt.EnvIdex], int(total_steps/1000))
 
+        env.close()
+        eval_env.close()
 
 if __name__ == '__main__':
     main()
